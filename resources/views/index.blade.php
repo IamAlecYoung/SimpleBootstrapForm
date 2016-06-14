abf620053379d23f22ee3a649d7a7bfa6eecc9ef
Alec <iamalecyoung@gmail.com>
2016-06-14 19:34:34 +0100

    Made navigation easier by changing routes names and views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('mainpage')
+
+@section('content')
+
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <h3>Send me a message</h3>
+            <form role="form" id="contactForm">
+
+            </form>
+        </div>
+      </div>
+
+@stop
---
+++ resources/views/mainpage.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Bootstrap Login</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- jQuery stylesheets -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+</head>
+<body>
+
+  @yield('header')
+
+  @yield('content')
+
+  @yield('footer')
+
+</body>
+    <!-- jQuery Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</html>
